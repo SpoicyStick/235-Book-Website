966cd8641cb04e34d591c7cf6f4043bf6e9a5265
cornyyumi <cornyyumi@gmail.com>
2021-09-10 19:31:24 +1200

    fixed HTTP errors and linked the books

--- books_blueprint/books.py
+++ books_blueprint/books.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Blueprint, render_template, url_for, request
 import library.adapters.repository as repo
 from library.domain.model import Book
@@ -24,22 +25,14 @@ def list_book():
         list_url = url_for('books_bp.list_book'),
         book_url=url_for('books_bp.simple_book')
     )
-@books_blueprint.route('/single_book', methods=['GET', 'POST'])
+@books_blueprint.route('/single_book', methods=['GET'])
 def simple_book():
-    book_id = int(request.form['book_id'])
-    a_book = Book(book_id, "Harry Potter and the Chamber of Secrets")
-    a_book.description = "Ever since Harry Potter had come home for the summer, the Dursleys had been so mean \
-                         and hideous that all Harry wanted was to get back to the Hogwarts School for \
-                         Witchcraft and Wizardry. But just as he’s packing his bags, Harry receives a \
-                         warning from a strange impish creature who says that if Harry returns to Hogwarts, \
-                         disaster will strike."
-    a_book.release_year = 1999
-    a_book.ebook = False
-    a_book.image = "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4088/9781408855669.jpg"
+    book_id = int(request.args.get('book_id'))
     return render_template(
         'simple_book.html',
-        book = a_book,
+        book = repo.repo_instance.get_book(book_id),
         home_url = url_for('books_bp.home'),
         list_url = url_for('books_bp.list_book'),
         book_url=url_for('books_bp.simple_book')
     )
+
